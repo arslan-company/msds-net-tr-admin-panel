19c8405205167d4a4502b13db12d8d5d61e6080d
Okan <okan.unallar@gmail.com>
2024-11-15 17:37:28 +0300

    build errors fixed

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next'
 
-import { PayloadRedirects } from '@/components/PayloadRedirects'
 import configPromise from '@payload-config'
 import { getPayloadHMR } from '@payloadcms/next/utilities'
 import { draftMode } from 'next/headers'
 import React, { cache } from 'react'
-import { homeStatic } from '@/endpoints/seed/home-static'
 
 // import type { Page as PageType } from '@/payload-types'
 
 import { RenderBlocks } from '@/blocks/RenderBlocks'
-import { RenderHero } from '@/heros/RenderHero'
 import { generateMeta } from '@/utilities/generateMeta'
 import PageClient from './page.client'
 
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,6 @@
-import PageTemplate, { generateMetadata } from './[slug]/page'
+// import PageTemplate, { generateMetadata } from './[slug]/page'
+import PageTemplate from './[slug]/page'
 
 export default PageTemplate
 
-export { generateMetadata }
+// export { generateMetadata }
